f21393bd551a498cb2e5aa1ef10b0792def23ab2
Naveneetha Vasudevan <naveneetha@yahoo.com>
2014-10-09 14:41:25 +0100

    use getopt to handle options for command line

--- src/AmbiMin.py
+++ src/AmbiMin.py
@@ -1,24 +1,59 @@
 #! /usr/bin/env python
 
-import os, sys, tempfile
+import os, sys, tempfile, getopt
 import Accent, Backends, CFG, Lexer
 import AmbiParse 
-import tempfile
+import tempfile, shutil
 
 
 class AmbiMin:
 
     def __init__(self):
-        self.gf = sys.argv[1]
-        self.lf = sys.argv[2]
-        self.backend = sys.argv[3]
-        self.t_depth = int(sys.argv[4])
-        self.wgt = float(sys.argv[5])
-        self.mincnt = int(sys.argv[6])
-        print "wgt: %s" % self.wgt
+        opts, args = getopt.getopt(sys.argv[1 : ], "hb:d:w:n:")
+        self.gf,self.lf = None, None
+        self.t_depth = None
+        self.backend = None
         self.wgt = None
+        self.mincnt = None
+        for opt in opts:
+            if opt[0] == "-b":
+                self.backend = opt[1]
+            elif opt[0] == "-d":
+                self.t_depth = int(opt[1])  
+            elif opt[0] == "-w":
+                self.wgt = float(opt[1])             
+            elif opt[0] == "-n":
+                self.mincnt = int(opt[1])
+            elif opt[0] == "-h":
+                self.usage()
+            else:
+                self.usage("Unknown argument '%s'" % opt[0])
+
+        if len(args) != 2:
+            self.usage()
+
+        self.gf,self.lf = args[0],args[1]
+
+        if self.backend is None:
+            self.usage("backend is not set")
+
+        if self.t_depth is None:
+            self.usage("depth is not set")
+
+        if self.mincnt is None:
+            self.usage("minimisation count is not set")
+
         self.minimise_ambiguity(self.gf, self.lf)
 
+
+    def usage(self, msg):
+        if msg is not None:
+            sys.stderr.write("\n%s\n" % msg)        
+            sys.stderr.write("python AmbiMin.py -b <backend> -d <depth" \
+                "-n <max minimisation count>" \
+                "-w <wgt to apply on reaching threshold depth>" \
+                "<grammar> <lex>")
+            sys.exit(1)
 
     def find_ambiguity(self, gp, lp):
         print "===> %s : %s" % (gp, self.backend)
@@ -67,18 +102,21 @@ class AmbiMin:
                 tokenline = l
                 
         currgp = gp
+        td = tempfile.mkdtemp()
         n = 1
         while n <= self.mincnt: 
             print "[%s]currgp: %s" % (str(n),currgp)
             is_amb, sen, acc_out = self.find_ambiguity(currgp, lp)
             assert is_amb
             mincfg = AmbiParse.parse(acc_out) 
-            new_gp = "%s.%s.acc" % (currgp.split('.')[0],str(n))
+            new_gp = os.path.join(td,"%s.acc" % str(n))
             self.write_cfg(mincfg, new_gp, tokenline)
             self.print_stats(currgp, sen)
             currgp = new_gp
             n += 1
 
+#        if os.path.exists(td):
+#            shutil.rmtree(td)
 
 
 AmbiMin()

4b94fa599f1af170dec38dbd32064c70367a6976
Naveneetha Vasudevan <naveneetha@yahoo.com>
2014-10-24 22:43:10 +0100

    Two changes: 1) Add option to save the minimised cfg; 2) Add license header

--- src/AmbiMin.py
+++ src/AmbiMin.py
@@ -1,14 +1,34 @@
 #! /usr/bin/env python
+#
+# Copyright (c) 2012 King's College London
+# created by Laurence Tratt and Naveneetha Vasudevan
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
 
-import os, sys, tempfile, getopt, shutil
-import Accent, Backends, CFG, Lexer
-import AmbiParse 
+
+import os, sys, getopt
 import Minimisers
 
 class AmbiMin:
 
     def __init__(self):
-        opts, args = getopt.getopt(sys.argv[1 : ], "hb:d:w:n:m:t:")
+        opts, args = getopt.getopt(sys.argv[1 : ], "hb:d:w:n:m:t:s")
         self.gf,self.lf = None, None
         self.t_depth = None
         self.backend = None
@@ -16,6 +36,8 @@ class AmbiMin:
         self.mincnt = 0
         self.minimiser = None
         self.duration = None
+        self.save_min_cfg = False
+
         for opt in opts:
             if opt[0] == "-b":
                 self.backend = opt[1]
@@ -29,6 +51,8 @@ class AmbiMin:
                 self.minimiser = opt[1]
             elif opt[0] == "-t":
                 self.duration = int(opt[1])
+            elif opt[0] == "-s":
+                self.save_min_cfg = True
             elif opt[0] == "-h":
                 self.usage()
             else:

04ab49d48a085fe432c5dae5747cf7945c7d9038
krish <krish.vasudevan@polestarglobal.com>
2016-04-26 15:34:24 +0100

    Add extended dynamic backends - insert minimised sentence

---
+++ src/Backends/Dynamic4m.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2012 King's College London
+# created by Laurence Tratt and Naveneetha Vasudevan
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+
+import Dynamic4
+import MinSen
+
+
+class Calc(Dynamic4.Calc, MinSen.Insert):
+    def __init__(self, sin, mincfg, minsen):
+        Dynamic4.Calc.__init__(self, sin)
+        MinSen.Insert.__init__(self, mincfg, minsen)
+
+
+    def _dive(self, rule, depth, wgt):
+        if (not self.found) and (rule.name == self.r): 
+            self._depth += 1
+            rule.entered += 1
+            self.found = True
+            print "found -> True"
+            self.insert_minsen(rule, depth, wgt)
+            rule.exited += 1
+            self._depth -= 1
+        else:
+            Dynamic4.Calc._dive(self, rule, depth, wgt)
---
+++ src/Backends/MinSen.py
@@ -0,0 +1,51 @@
+# Copyright (c) 2012 King's College London
+# created by Laurence Tratt and Naveneetha Vasudevan
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+import CFG
+import Utils
+
+
+class Insert(object):
+
+    def __init__(self, mincfg, minsen):
+        self.subseq = mincfg.get_rule('root').seqs[0] # take out [0] for all seqs
+        self.r, self.seqi, self.symi = Utils.find_rule(self._cfg, self.subseq)
+        self.minsen = minsen.split()
+        print "=> %s, %s, %s => %s" % (self.r, self.seqi, self.symi, self.subseq)
+        print "=> minsen: %s" % self.minsen
+        self.found = False
+
+
+    def insert_minsen(self, rule, depth, wgt=None):
+        seq = rule.seqs[self.seqi]
+        j = 0
+        while j < len(seq):
+            if (j < self.symi) or (j >= (self.symi+len(self.subseq))):
+                e = seq[j]
+                if isinstance(e, CFG.Non_Term_Ref):
+                    self._dive(self._cfg.get_rule(e.name), depth, wgt)
+                else:
+                    self._s.append(self._cfg.gen_token(e.tok))
+                j += 1
+            else:
+                self._s += self.minsen
+                j += len(self.subseq) 
+

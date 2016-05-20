9de58ba3c9dc59e6e73d2da70a2e2113b17c75bc
krish <krish.vasudevan@polestarglobal.com>
2016-05-20 10:59:03 +0100

    setup travis to run an ambiguity test

---
+++ bin/install_deps.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -x
+
+apt-get update -q
+apt-get install -y gcc flex wget make patch
---
+++ bin/test_sinbad.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+
+# run sinbad against a example grammar
+
+# build ACCENT
+wget http://accent.compilertools.net/accent.tar -O ~/accent.tar
+tar xf ~/accent.tar -C ~/
+(cd ~/accent/accent && ./build)
+
+# apply patch for entire -- essentially sets up an exit code
+wget https://raw.githubusercontent.com/nvasudevan/experiment/master/patches/entire.c.patch -O ~/entire.c.patch
+patch -b -p0 ~/accent/entire/entire.c < ~/entire.c.patch
+
+export ACCENT_DIR=${HOME}/accent
+echo $ACCENT_DIR
+
+# run sinbad against various backends on an example grammar
+python ../src/sinbad -b dynamic1 -d 10 bin/amb2.acc bin/general.lex

5eada37470b0b714f1e994dc5f814c86dde52d42
krish <naveneetha@yahoo.com>
2017-04-14 14:16:12 +0100

    Fix travis checks too

--- bin/test_sinbad.sh
+++ bin/test_sinbad.sh
@@ -6,8 +6,7 @@ echo $ACCENT_DIR
 
 # run sinbad against various backends on an example grammar
 python src/sinbad -b dynamic1 -d 10 bin/amb2.acc bin/general.lex
+python src/sinbad -b dynamic2 -d 10 bin/amb2.acc bin/general.lex
 python src/sinbad -b dynamic3 -d 10 bin/amb2.acc bin/general.lex
-python src/sinbad -b dynamic4 -d 10 -w 0.1 bin/amb2.acc bin/general.lex
-python src/sinbad -b dynamic7 -d 10 -w 0.1 bin/amb2.acc bin/general.lex
-python src/sinbad -b dynamic11 -d 10 -w 0.1 bin/amb2.acc bin/general.lex
-python src/sinbad -b dynamic12 -d 10 -w 0.1 bin/amb2.acc bin/general.lex
+python src/sinbad -b dynamic1rws -d 10 -w 0.1 bin/amb2.acc bin/general.lex
+python src/sinbad -b dynamic2rws -d 10 -w 0.1 bin/amb2.acc bin/general.lex
